adc21fa75ee25320b6a232484e40ccf6997cbabb
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-10-03 19:22:04 +0800

    added default accounts

--- app/Http/Controllers/Api/AccountsController.php
+++ app/Http/Controllers/Api/AccountsController.php
@@ -3,28 +3,36 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Services\AccountsService;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use App\Services\AccountsService;
+use App\Http\Requests\Accounts\CreateAccountRequest;
 
 class AccountsController extends Controller
 {
     public function __construct(
         private readonly AccountsService $accountsService
     ) {}
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() : JsonResponse
     {
-        //
+        $result = $this->accountsService->getList();
+
+        return response()->json($result, Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateAccountRequest $request)
     {
-        //
+        $result = $this->accountsService->create($request->validated());
+
+        return response()->json($result, Response::HTTP_CREATED);
     }
 
     /**
