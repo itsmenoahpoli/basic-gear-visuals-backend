1bf0fb9ca45d2544190aab0b5b8f7cd7f1362a6f
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-10-22 11:18:22 +0800

    teachers/students default accounts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersSeeder::class,
             SubjectsSeeder::class,
+            StudentsSeeder::class,
+            TeachersSeeder::class
         ]);
     }
 }
