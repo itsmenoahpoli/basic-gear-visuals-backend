ad0437e37e1bde4fe1545fd577889127bbdcc278
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-10-23 09:06:31 +0800

    upload submit lab

--- app/Services/LaboratorySubmissionsService.php
+++ app/Services/LaboratorySubmissionsService.php
@@ -7,13 +7,30 @@ use App\Repositories\LaboratorySubmissionsRepository;
 
 class LaboratorySubmissionsService extends LaboratorySubmissionsRepository
 {
-    public function __construct(LaboratorySubmission $model)
+    public function __construct(private LaboratorySubmission $model)
     {
         parent::__construct($model, [], []);
     }
 
     public function submitLaboratory($payload)
     {
-        return $payload;
+        if (count($payload['files']))
+        {
+            $uploadedFiles = [];
+
+            foreach ($payload['files'] as $file) {
+                $filename = $file->getClientOriginalName();
+                $file->move(public_path('uploads/laboratory-submissions'), $filename);
+                $uploadedFiles[] = '/uploads/laboratory-submissions/'.$filename;
+            }
+
+
+            return $this->model->query()->create([
+                'user_id' => $payload['user_id'],
+                'lecture_id' => $payload['lecture_id'],
+                'quiz_score' => $payload['quiz_score'],
+                'laboratories_data' => json_encode($uploadedFiles)
+            ]);
+        }
     }
 }
